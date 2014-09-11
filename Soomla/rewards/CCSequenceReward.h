e81e220d1b80d32ae8ef9104ece56a0d2174bf7e
vedi <shubin.fedor@gmail.com>
2014-09-11 13:10:45 +0400

    remove schedule from CCSequenceReward creating

--- Soomla/rewards/CCSequenceReward.h
+++ Soomla/rewards/CCSequenceReward.h
@@ -21,8 +21,7 @@ namespace soomla {
         static CCSequenceReward *create(
                 cocos2d::__String *id,
                 cocos2d::__String *name,
-                cocos2d::__Array *rewards,
-                CCSchedule *schedule
+                cocos2d::__Array *rewards
         );
 
         SL_CREATE_WITH_DICTIONARY(CCSequenceReward);
