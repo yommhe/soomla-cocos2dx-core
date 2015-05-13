b8042d7c370c3d356c5e39f4dc70280836a8d4b1
vedi <shubin.fedor@gmail.com>
2015-05-13 16:12:58 +0700

    fix android compilation

--- Android.mk
+++ Android.mk
@@ -45,6 +45,11 @@ LOCAL_EXPORT_C_INCLUDES += $$(LOCAL_PATH)/Soomla/jsb
 endif
 
 include $(BUILD_STATIC_LIBRARY)
+
+ifneq '$(COCOS2D_JAVASCRIPT)' ''
 $(call import-module,jansson)
 $(call import-module,scripting/js-bindings/proj.android/prebuilt-mk)
 $(call import-module,tools/simulator/libsimulator/proj.android/prebuilt-mk)
+else
+$(call import-module,external/jansson)
+endif
